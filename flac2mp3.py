e0cb6564c47b0b52f42cf8c1fb9f1caceabaec96
costagabbie <102681995+costagabbie@users.noreply.github.com>
2023-09-26 09:56:06 -0300

    Now accepting an output path for more convenience

--- flac2mp3.py
+++ flac2mp3.py
@@ -6,12 +6,13 @@ from sys import argv
 
 def main():
     files = [f for f in listdir(argv[1]) if path.isfile(path.join(argv[1], f))]
-    dest_dir = path.dirname(argv[1])
+    src_dir = path.dirname(argv[1])
+    dest_dir = path.dirname(argv[2])
     for file in files:
         if file.split('.')[1] == 'flac':
             converted_file = file.split('.')[0]+'.mp3'
-            print(f'Converting {path.join(dest_dir,file)} to file {path.join(dest_dir,converted_file)}')
-            check_output(f'ffmpeg -i "{path.join(dest_dir,file)}" -ab 320k -map_metadata 0 -id3v2_version 3 "{path.join(dest_dir,converted_file)}"',shell=True)
+            print(f'Converting {path.join(src_dir,file)} to file {path.join(src_dir,converted_file)}')
+            check_output(f'ffmpeg -i "{path.join(src_dir,file)}" -ab 320k -map_metadata 0 -id3v2_version 3 "{path.join(dest_dir,converted_file)}"',shell=True)
         else:
             print(f'Skipping {file} (not a flac)')
     print('All done')
