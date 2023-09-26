13be4827f009ea9e97695ad9b89553c8901b72b5
costagabbie <102681995+costagabbie@users.noreply.github.com>
2023-09-26 09:23:35 -0300

    First Commit

---
+++ flac2mp3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from os import path, listdir
+from subprocess import check_output
+from sys import argv
+
+
+def main():
+    files = [f for f in listdir(argv[1]) if path.isfile(path.join(argv[1], f))]
+    dest_dir = path.dirname(argv[1])
+    for file in files:
+        if file.split('.')[1] == 'flac':
+            converted_file = file.split('.')[0]+'.mp3'
+            print(f'Converting {path.join(dest_dir,file)} to file {path.join(dest_dir,converted_file)}')
+            check_output(f'ffmpeg -i "{path.join(dest_dir,file)}" -ab 320k -map_metadata 0 -id3v2_version 3 "{path.join(dest_dir,converted_file)}"',shell=True)
+        else:
+            print(f'Skipping {file} (not a flac)')
+    print('All done')
+
+if __name__ == '__main__':
+    main()
